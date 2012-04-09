ce13577bc69772c9106a7455b505d09e4eda4775
Robin Gersabeck <Code6226@gmail.com>
2012-04-09 01:04:28 -0400

    Added player sprite, basic response to input

--- src/com/puchisoft/WalkingAroundOnline.java
+++ src/com/puchisoft/WalkingAroundOnline.java
@@ -1,7 +1,11 @@
 package com.puchisoft;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
@@ -10,6 +14,10 @@ public class WalkingAroundOnline implements ApplicationListener{
 
 	SpriteBatch spriteBatch;
 	BitmapFont font;
+	
+	Texture texturePlayer;
+	Vector2 playerPosition = new Vector2(50, 50);
+	
 	Vector2 textPosition = new Vector2(50, 200);
 	
 	@Override
@@ -17,13 +25,16 @@ public class WalkingAroundOnline implements ApplicationListener{
 		font = new BitmapFont();
 		font.setColor(Color.GREEN);
 		font.setScale(2.0f);
+		
+		texturePlayer = new Texture(Gdx.files.internal("data/player.png"));
+		
 		spriteBatch = new SpriteBatch();
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		texturePlayer.dispose();
+		spriteBatch.dispose();
 	}
 
 	@Override
@@ -34,9 +45,29 @@ public class WalkingAroundOnline implements ApplicationListener{
 
 	@Override
 	public void render() {
+		
+		if (Gdx.input.isKeyPressed(Keys.W)) {
+			playerPosition.y += 3;
+		}
+		else if(Gdx.input.isKeyPressed(Keys.S)) {
+			playerPosition.y -= 3;
+		}
+		if (Gdx.input.isKeyPressed(Keys.A)) {
+			playerPosition.x -= 3;
+		}
+		else if(Gdx.input.isKeyPressed(Keys.D)) {
+			playerPosition.x += 3;
+		}
+		
 		spriteBatch.begin();
 		spriteBatch.setColor(Color.WHITE);
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
 		font.draw(spriteBatch, "Hello World", (int)textPosition.x, (int)textPosition.y);
+		
+		spriteBatch.draw(texturePlayer, playerPosition.x, playerPosition.y, 0, 0, texturePlayer.getWidth(),
+				texturePlayer.getHeight());
 		spriteBatch.end();
 	}
 
