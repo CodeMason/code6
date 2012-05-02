6f6980bf25cd090f8d49c973806ffd9add22e997
James Cash <james.cash@case.edu>
2012-05-02 15:41:02 -0400

    Added gravity between players and asteroids.

--- MultiplayerSpaceGame/src/com/puchisoft/multiplayerspacegame/GameMap.java
+++ MultiplayerSpaceGame/src/com/puchisoft/multiplayerspacegame/GameMap.java
@@ -176,7 +176,7 @@ public class GameMap {
 						}
 						Vector2 gravityVector = moonChase.getPosition().cpy().sub(playerEntryMoon.getValue().moon.getPosition()).nor().mul(gravityForce);
 						playerEntryMoon.getValue().moon.velocity.add(gravityVector.mul(delta));
-						Log.info(String.valueOf(gravityForce));
+						//Log.info(String.valueOf(gravityForce));
 					}
 				}
 				//Gravity on Players from Chase Moon (Gravity Well)
@@ -186,7 +186,7 @@ public class GameMap {
 				}
 				Vector2 gravityVector = moonChase.getPosition().cpy().sub(playerEntryMoon.getValue().moon.getPosition()).nor().mul(gravityForce);
 				playerEntryMoon.getValue().moon.velocity.add(gravityVector.mul(delta));
-				Log.info(String.valueOf(gravityForce));
+				//Log.info(String.valueOf(gravityForce));
 			}
 			//Gravity on Bullets from Moons
 			for (int i = 0; i < bullets.size(); i++) {
@@ -204,6 +204,22 @@ public class GameMap {
 							.mul(gravityForce);
 					bulletCur.setVelocity(bulletCur.getVelocity().add(
 							gravityVector.mul(delta)));
+					//Log.info(String.valueOf(gravityForce));
+				}
+
+			}
+			//Gravity on Players from Asteroids
+			for (int j = 0; j < asteroids.size(); j++) {
+				Asteroid asteroidCurJ = asteroids.get(j);
+				gravityForce = 0;
+				if (!playerEntry.getValue().moon.getPosition().equals(asteroidCurJ.getPosition())) {
+					gravityForce = (gravityAsteroid / playerEntry.getValue().moon.getPosition()
+							.dst2(asteroidCurJ.getPosition()));
+					if (gravityForce > 10) {
+						gravityForce = 10;
+					}
+					Vector2 gravityVector = asteroidCurJ.getPosition().cpy().sub(playerEntry.getValue().moon.getPosition()).nor().mul(gravityForce);
+					playerEntry.getValue().moon.velocity.add(gravityVector.mul(delta));
 					Log.info(String.valueOf(gravityForce));
 				}
 
@@ -225,7 +241,7 @@ public class GameMap {
 					}
 					Vector2 gravityVector = bulletCurJ.getPosition().cpy().sub(bulletCurI.getPosition()).nor().mul(gravityForce);
 					bulletCurI.setVelocity(bulletCurI.getVelocity().add(gravityVector.mul(delta)));
-					Log.info(String.valueOf(gravityForce));
+					//Log.info(String.valueOf(gravityForce));
 				}
 
 			}
@@ -241,7 +257,7 @@ public class GameMap {
 					}
 					Vector2 gravityVector = asteroidCurJ.getPosition().cpy().sub(bulletCurI.getPosition()).nor().mul(gravityForce);
 					bulletCurI.setVelocity(bulletCurI.getVelocity().add(gravityVector.mul(delta)));
-					Log.info(String.valueOf(gravityForce));
+					//Log.info(String.valueOf(gravityForce));
 				}
 
 			}
